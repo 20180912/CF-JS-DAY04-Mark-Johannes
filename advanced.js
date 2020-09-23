835eeb2fec44dd934e1267cffde2ec4429791208
Johannes Zarre <zarre.johannes@gmail.com>
2020-09-23 14:26:10 +0200

    push to mark

--- advanced.js
+++ advanced.js
@@ -1,6 +1,8 @@
 
+function incircle() {
+	document.getElementById('demo').innerHTML = 'you are in the circle';
 
-
-function mouseOver {
-	document.getElementById("demo").innerHTML = "you are in the circle"
 }
+
+var elementNode = document.getElementById('circle');
+elementNode.addEventListener('click',incircle,false)
