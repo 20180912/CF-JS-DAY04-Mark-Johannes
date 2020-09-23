470343809337dcf9988498d583cf7d8f426f9627
Mark.Etzel@gmx.email <Mark.Etzel@gmx.de>
2020-09-23 14:37:58 +0200

    Advanced Exercise

--- advanced.js
+++ advanced.js
@@ -1,8 +1,19 @@
 
 function incircle() {
-	document.getElementById('demo').innerHTML = 'you are in the circle';
-
+	document.getElementById('demo').innerHTML = "you are in the circle";
+}
+function outcircle() {
+	document.getElementById('demo').innerHTML = "you are outside of the circle";
+}
+function click() {
+	document.body.style.backgroundColor= 'grey';
+}
+function doubleclick() {
+	document.body.style.backgroundColor= 'blue';
 }
 
 var elementNode = document.getElementById('circle');
-elementNode.addEventListener('click',incircle,false)
+elementNode.addEventListener('mouseover', incircle, false);
+elementNode.addEventListener('mouseout', outcircle, false);
+elementNode.addEventListener('click', click, false);
+elementNode.addEventListener('dblclick', doubleclick, false);
