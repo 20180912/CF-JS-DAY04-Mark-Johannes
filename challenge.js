9eb18cefae8eaf5559d99c9669a28ebbc3e6b261
Johannes Zarre <zarre.johannes@gmail.com>
2020-09-23 15:15:07 +0200

    basic structure

--- challenge.js
+++ challenge.js
@@ -13,10 +13,25 @@ class quiz {
 		this.answer3=d;
 		this.correctAnswer=e;
 	}
+populate(){
+	document.getElementById('questionBox').innerHTML = this.question
+	document.getElementById('answerBox1').innerHTML = this.answer1
+	document.getElementById('answerBox2').innerHTML = this.answer2
+	document.getElementById('answerBox3').innerHTML = this.answer3
+	document.getElementById('answerBox4').innerHTML = this.correctAnswer
+}
+	
+
 }
 
+document.getElementById('questionBox').innerHTML
 
 var question1 = new quiz("What is the correct Answer","This is Button A","This is Button B","This is Button C", "This is Button D")
 
 
 console.table(question1);
+
+question1.populate();
+
+
+
